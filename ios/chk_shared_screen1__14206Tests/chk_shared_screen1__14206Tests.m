df4b870675657c9d239a01158e0c4a0c6a6bd592
Crowdbotics <team@crowdbotics.com>
2020-10-29 12:40:54 +0000

    Initial commit.

---
+++ ios/chk_shared_screen1__14206Tests/chk_shared_screen1__14206Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface chk_shared_screen1__14206 : XCTestCase
+
+@end
+
+@implementation chk_shared_screen1__14206
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
